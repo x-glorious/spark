bff085c1357e9769483599e4cded975f950b1b1c
mayfly <59794462+x-glorious@users.noreply.github.com>
2023-12-19 22:28:41 +0800

    fix: reoslve build error

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,21 +1,21 @@
 import { createIntl, createIntlCache } from 'react-intl'
 import { enMessages } from './en'
-import { cnMessages } from './cn'
+// import { cnMessages } from './cn'
 
 // This is optional but highly recommended
 // since it prevents memory leak
 const cache = createIntlCache()
 
 export const intl = createIntl(
-  // {
-  //   locale: 'en-Us',
-  //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  //   messages: enMessages as any,
-  // },
   {
-    locale: 'zh-Cn',
+    locale: 'en-Us',
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    messages: cnMessages as any,
+    messages: enMessages as any,
   },
+  // {
+  //   locale: 'zh-Cn',
+  //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  //   messages: cnMessages as any,
+  // },
   cache,
 )
