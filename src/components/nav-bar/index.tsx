0da5ec0e65b42dd06df7901d058a22cd8527aaa3
mayfly <59794462+x-glorious@users.noreply.github.com>
2023-12-23 15:51:48 +0800

    feat: add nav-bar slogan to home

--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -6,11 +6,19 @@ import { LuLogIn } from 'react-icons/lu'
 import { Avatar } from './avatar'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { Language } from './language'
+import { useCallback } from 'react'
 
 export const NavBar = () => {
   const user = useDetailStore((state) => state.value)
   const location = useLocation()
   const navigate = useNavigate()
+
+  const toHome = useCallback(() => {
+    if (location.pathname !== '/') {
+      navigate('/')
+    }
+  }, [location, navigate])
+
   if (['/oauth/login'].includes(location.pathname)) {
     return null
   }
@@ -20,7 +28,7 @@ export const NavBar = () => {
       <Flex height="100%" alignItems="center" h={14} px={4} py={4}>
         <Flex alignItems="center" gap={1}>
           <AppIcon size={10} />
-          <Slogan size={6} />
+          <Slogan size={6} onClick={toHome} cursor="pointer" />
         </Flex>
         <Flex flex={1} justifyContent="flex-end" alignItems="center" gap={4}>
           <Language />
--- src/components/slogan/index.tsx
+++ src/components/slogan/index.tsx
@@ -1,8 +1,8 @@
 import { useIntlFc } from '@/hooks/use-intl-fc'
 import { i18nDefine } from '@/i18n/define'
-import { Box } from '@chakra-ui/layout'
+import { Box, BoxProps } from '@chakra-ui/layout'
 
-export interface SloganProps {
+export interface SloganProps extends BoxProps {
   /**
    * as root height
    */
@@ -14,11 +14,11 @@ export interface SloganProps {
 }
 
 export const Slogan = (props: SloganProps) => {
-  const { color = 'purple.800', size } = props
+  const { color = 'purple.800', size, ...others } = props
   const Slogan = useIntlFc(i18nDefine.basic.slogan)
 
   return (
-    <Box h={size} color={color}>
+    <Box h={size} color={color} {...others}>
       <Slogan />
     </Box>
   )
